d8a5d9e3f37795dbd4d1ea0b39356b13818b837a
Netology Test <elen.1996@list.ru>
2025-08-20 13:39:55 +0300

    fixed2 tests

--- puppeteer-tickets/tests/booking.test.js
+++ puppeteer-tickets/tests/booking.test.js
@@ -1,20 +1,12 @@
 const puppeteer = require('puppeteer');
 const {clickElement, getText, putText} = require ("./commands");
 
-// const { 
-//   selectDay, 
-//   selectMovie, 
-//   selectHall, 
-//   selectSeat,
-//  // openAcinemaHallAndAsession 
-// } =  require('./commands');
-
 describe('Ticket Booking', () => {
   let browser;
   let page;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: false });
+    browser = await puppeteer.launch({ headless: false,  slowMo: 50 });
     page = await browser.newPage();
     await page.goto('https://qamid.tmweb.ru/client/index.php');
   });
@@ -23,17 +15,7 @@ describe('Ticket Booking', () => {
     await browser.close();
   });
 
-// test('Buying one ticket', async () => {
-//     await selectDay(page);
-//     await selectMovie(page);
-//     await selectHall(page);
-//     await selectSeat(page, 4);
-//     await page.click('.acceptin-button');
-    
-//     await page.waitForSelector('.buying-scheme__chair.buying-scheme__chair_standart.buying-scheme__chair_selected');
-//   });
-
-test.skip('Buying one ticket', async () => {
+test('Buying one ticket', async () => {
   const expected = 'Вы выбрали билеты:';
   
   await clickElement(page,".movie-seances__time:not(.acceptin-button-disabled)");
@@ -49,9 +31,9 @@ test('Buying three tickets', async () => {
   const expected = 'Вы выбрали билеты:';
   
   await clickElement(page,".movie-seances__time:not(.acceptin-button-disabled)");
-  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(6):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(7):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(8):not(.buying-scheme__chair_taken)');
+  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(2):not(.buying-scheme__chair_taken)');
+  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(3):not(.buying-scheme__chair_taken)');
+  await clickElement(page,'.buying-scheme__row:nth-child(1) .buying-scheme__chair:nth-child(4):not(.buying-scheme__chair_taken)');
 
   await clickElement(page,'.acceptin-button');
   const actual = await getText(page,'.ticket__check-title');
@@ -62,26 +44,17 @@ test('Buying three tickets', async () => {
 test('Sad Path: Buying an occupied seat', async () => {
   
   await clickElement(page,".movie-seances__time:not(.acceptin-button-disabled)");
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(6):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(7):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(8):not(.buying-scheme__chair_taken)');
+  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(5):not(.buying-scheme__chair_taken)');
+  // await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(7):not(.buying-scheme__chair_taken)');
+  // await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(8):not(.buying-scheme__chair_taken)');
   await clickElement(page,".acceptin-button");
   await clickElement(page,".acceptin-button");
 
   await page.goto('https://qamid.tmweb.ru/client/index.php');
-  await clickElement(page,".movie-seances__time:not(.acceptin-button-disabled)");
-  
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(6):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(7):not(.buying-scheme__chair_taken)');
-  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(8):not(.buying-scheme__chair_taken)');
+  await clickElement(page,".movie-seances__time:not(.acceptin-button-disabled)"); 
+  await clickElement(page,'.buying-scheme__row:nth-child(2) .buying-scheme__chair:nth-child(5)'); 
 
-  await clickElement(page,'.acceptin-button-disabled');
-  const expected = '.acceptin-button-disabled';
   const isButtonDisabled = await page.$eval('.acceptin-button', btn => btn.disabled);
-
-  // const actual = await getText(page,'.ticket__check-title');
-
-  // expect(actual.trim()).toBe(expected);
 
   expect(isButtonDisabled).toBe(true);
   }, 30000);
